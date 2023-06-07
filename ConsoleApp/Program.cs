7785d6a9a6d5c9e35e347010442c036223905d92
youn1on <a.shymanska.ip13@kpi.ua>
2023-06-07 15:41:31 +0300

    User interface added :)

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Interfaces;
+using Infrastructure.Models.Handlers;
+
+namespace ConsoleApp;
+
+public static class Program
+{
+    public static void Main()
+    {
+        IHandler handlersChain = ConstructHandlers();
+        UserInterface.MainLoop(handlersChain);
+    }
+
+    public static IHandler ConstructHandlers()
+    {
+        IHandler firstHandler = new SqlInjectionsHandler();
+        IHandler xssHandler = new XssInjectionsHandler();
+        firstHandler.SetNextHandler(xssHandler);
+        IHandler nameHandler = new NameValidationHandler();
+        xssHandler.SetNextHandler(nameHandler);
+        IHandler surnameHandler = new SurnameValidationHandler();
+        nameHandler.SetNextHandler(surnameHandler);
+        IHandler patronymicHandler = new PatronymicValidationHandler();
+        surnameHandler.SetNextHandler(patronymicHandler);
+        IHandler emailHandler = new EmailValidationHandler();
+        patronymicHandler.SetNextHandler(emailHandler);
+        IHandler phoneNumberHandler = new PhoneNumberValidationHandler();
+        emailHandler.SetNextHandler(phoneNumberHandler);
+        IHandler deliveryHandler = new DeliveryTypeValidationHandler();
+        phoneNumberHandler.SetNextHandler(deliveryHandler);
+        IHandler addressHandler = new AddressValidationHandler();
+        deliveryHandler.SetNextHandler(addressHandler);
+        IHandler postOfficeNumberHandler = new PostOfficeValidationHandler();
+        addressHandler.SetNextHandler(postOfficeNumberHandler);
+
+        return firstHandler;
+    }
+}
---
+++ ConsoleApp/UserInterface.cs
@@ -0,0 +1,84 @@
+﻿using Infrastructure.Interfaces;
+using Infrastructure.Models;
+
+namespace ConsoleApp;
+
+public static class UserInterface
+{
+    public static void MainLoop(IHandler handlerChain)
+    {
+        while (true)
+        {
+            FormData? form = FillNewForm();
+            if (form is null)
+                break;
+
+            var validationError = handlerChain.Handle(form);
+            if (validationError is null)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Successfully validated!");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Validation error!");
+                Console.WriteLine(validationError);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Exiting...");
+        Console.ResetColor();
+    }
+
+    private static FormData? FillNewForm()
+    {
+        string? name = GetInput("name");
+        if (name is null) return null;
+
+        string? surname = GetInput("surname");
+        if (surname is null) return null;
+        
+        string? patronymic = GetInput("patronymic");
+        if (patronymic is null) return null;
+        
+        string? phoneNumber = GetInput("phone number");
+        if (phoneNumber is null) return null;
+        
+        string? email = GetInput("email");
+        if (email is null) return null;
+        
+        string? deliveryType = GetInput("delivery type");
+        if (deliveryType is null) return null;
+        
+        string? address = null, postOffice = null;
+        if (deliveryType.Trim() != "SelfPickup")
+        {
+            address = GetInput("address");
+            if (address is null) return null;
+
+            if (deliveryType.Trim() != "ByPost")
+            {
+                postOffice = GetInput("post office number");
+                if (postOffice is null) return null;
+            }
+        }
+
+        return new FormData(name, surname, patronymic, phoneNumber, email, deliveryType, address, postOffice);
+    }
+
+    private static string? GetInput(string fieldName)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Enter the {fieldName} or 'q' to exit: ");
+        Console.ResetColor();
+        
+        var field = Console.ReadLine()!;
+        return field == "q" ? null : field;
+    }
+}
